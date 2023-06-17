4f3920f9319f255e995440edd3841834bb271a83
Mohamed Hossam <81392900+mohamedhoss123@users.noreply.github.com>
2023-06-17 14:48:12 +0300

    Init backend files

---
+++ backend/routes/home.router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import HomeController from "../controllers/home.controller.js";
+const router = Router()
+
+
+router.get("/",HomeController.index)
+
+
+export default router
