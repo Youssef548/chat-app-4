5d7d7f2c01b874b0d310d394e72b20a8b2801eda
Youssef548 <youssefahmedpvp@gmail.com>
2023-06-23 21:29:09 +0300

    make controller in back end return object messge

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,55 +1,56 @@
-import passport from "passport";
-import userModel from "../models/user.model.js";
+import passport from 'passport';
+import userModel from '../models/user.model.js';
 
 class authController {
-
   static async loginPost(req, res, next) {
-    passport.authenticate("local", (err, user, info) => {
-      if (err) throw err
+    passport.authenticate('local', (err, user, info) => {
+      if (err) throw err;
       if (!user) {
-        res.status(400).send("something wrong with username or password")
+        res.status(400).send({
+          type: 'error',
+          message: 'something wrong with username or password',
+        });
+      } else {
+        req.logIn(user, (err) => {
+          if (err) throw err;
+          res.send('Sucssfuly authed');
+        });
       }
-      else {
-        req.logIn(user, err => {
-          if (err) throw err
-          res.send("Sucssfuly authed")
-        })
-      }
-
-    })(req, res, next)
-  };
-
+    })(req, res, next);
+  }
 
   static async signupPost(req, res) {
-    const { username, password } = req.body
-    const data = await userModel.findOne({ username })
+    const { username, password } = req.body;
+    const data = await userModel.findOne({ username });
     if (data == null) {
-      const userdata = new userModel()
-      userdata.username = username
-      userdata.password = password
-      await userdata.save()
+      const userdata = new userModel();
+      userdata.username = username;
+      userdata.password = password;
+      await userdata.save();
 
       res.send(200);
     } else {
-      res.status(400).send({ type: "error", message: "username already taken" });
+      res
+        .status(400)
+        .send({ type: 'error', message: 'username already taken' });
     }
-  };
+  }
 
   static async validate(req, res) {
     if (!req.isAuthenticated()) {
-
-      res.status(401).send({ isAuthenticated: req.isAuthenticated() })
-      return
+      res.status(401).send({ isAuthenticated: req.isAuthenticated() });
+      return;
     }
-    res.status(200).send({ isAuthenticated: req.isAuthenticated() })
+    res.status(200).send({ isAuthenticated: req.isAuthenticated() });
   }
   static async logout(req, res) {
-    req.logout(function(err) {
-      if (err) { return next(err); }
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
       res.sendStatus(200);
     });
   }
-
 }
 
 export default authController;
