ec5d801029b46f954484d7978e4ff4de51dfa121
Mohamed Hossam <81392900+mohamedhoss123@users.noreply.github.com>
2023-06-25 09:57:28 +0300

    fix find friends and edit model property name

--- backend/controllers/friend.controller.js
+++ backend/controllers/friend.controller.js
@@ -3,11 +3,30 @@ import messagesModel from "../models/messages.model.js";
 class friendController{
   static async getAllUsers(req,res){
     let data = await messagesModel.aggregate([
-      { $match: { reciver: req.session.passport.user } },
-      { $group: { _id: "$sender", latestDate: { $max: "$date" } } },
-      { $sort: { latestDate: -1 } }
-    ]).exec()
-      console.log(data);
+      {
+        $match: {
+          $or: [
+            { receiver: req.session.passport.user },
+            { sender: req.session.passport.user }
+          ]
+        }
+      },
+      {
+        $group: {
+          _id: {
+            $cond: [
+              { $eq: ["$receiver", req.session.passport.user] },
+              "$sender",
+              "$receiver"
+            ]
+          },
+          latestDate: { $max: "$date" }
+        }
+      },
+      { $sort: { latestDate: -1 } },
+      
+    ]).exec();
+    console.log(data);
     res.send(data)
   }
 }
--- backend/gateway/events/sendMessage.js
+++ backend/gateway/events/sendMessage.js
@@ -5,12 +5,12 @@ import socketModel from "../../models/socket.model.js";
 export default async function (io, socket, args) {
     let message = messagesModel({
         sender: socket.request.session.passport.user,
-        reciver: args.reciver,
+        receiver: args.receiver,
         data: args.data,
         date: new Date()
     })
     let response = await message.save()
-    let socketData = await socketModel.find({ username: response.reciver })
+    let socketData = await socketModel.find({ username: response.receiver })
     socketData.forEach(element => {
         io.to(element.socketID).emit('recive-data', { data: response.data, sender: response.sender });
     });
