cfccd66caf4dd029cb9b4506d042758884b1f386
Youssef548 <youssefahmedpvp@gmail.com>
2023-06-30 17:31:02 +0300

    fix mohamed hossam mistake in backend (:

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,50 +1,24 @@
-import messagesModel from "../models/messages.model.js";
+import messagesModel from '../models/messages.model.js';
 
 class messageController {
   static async fetch(req, res) {
-    const limit = 15
-    let user = req.session.passport.user
-    let { username, page } = req.query
-    let messages = await messagesModel.find({
-      $or: [
-        { sender: user, receiver: username },
-        { sender: username, receiver: user }
-      ]
-    })
+    const limit = 15;
+    let user = req.session.passport.user;
+    let { username, page } = req.query;
+    let messages = await messagesModel
+      .find({
+        $or: [
+          { sender: user, receiver: username },
+          { sender: username, receiver: user },
+        ],
+      })
       .sort({ date: -1 })
-      .skip((page) * limit)
+      .skip(page * limit)
       .limit(limit)
-      .exec()
+      .exec();
     console.log(messages);
-    res.send(messages)
-  };
-
+    res.send(messages);
+  }
 }
 
 export default messageController;
-
-[
-  {
-    _id: new ObjectId("649c9978ede13b7848421c76"),
-    sender: 'wwwwww',
-    receiver: 'aaaaaa',
-    date: "2023-06-28T20:35:04.402Z",
-    data: 'wrqrwqrqw',
-    __v: 0
-  },
-  {
-    _id: new ObjectId("649c9973ede13b7848421c73"),
-    sender: 'wwwwww',
-    receiver: 'aaaaaa',
-    date: "2023-06-28T20:34:59.027Z",
-    data: 'wrqrwqrqw',
-    __v: 0
-  },
-  {
-    _id: new ObjectId("649c9970ede13b7848421c70"),
-    sender: 'wwwwww',
-    receiver: 'aaaaaa',
-    date: "2023-06-28T20:34:56.707Z",
-    data: 'wrqrwqrqw',
-    __v: 0
-  }]
